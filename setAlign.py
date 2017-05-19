2a495cc2d37fcb5b9eb479201cb88e164dd8ae39
Simon <saintexupery@snu.ac.kr>
2017-05-19 12:14:09 +0900

    setAlign function

--- setAlign.py
+++ setAlign.py
@@ -1,5 +1,26 @@
+import math
+
 def setAlign(n, k):
     answer = []
+    align = [i for i in range(1, n+1)] # align = list(range(1,n+1))
+
+    while n > 1:
+        factorial = math.factorial(n-1)
+        quotient = int(k / factorial) # quotient = k // factorial
+        remainder = k % factorial
+
+        if remainder == 0:
+            answer.append(align.pop(quotient-1))
+
+            while n > 1:
+                answer.append(align.pop())
+                n -= 1
+
+        else:
+            answer.append(align.pop(quotient))
+
+            k = remainder
+            n -= 1
 
     return answer
 
